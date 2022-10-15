44ffd483bf6bcbc6e8ba168a5ab1058d2906f6b4
LittleT <1211148525@qq.com>
2022-10-15 14:56:13 +0800

    update

--- src/main/java/reggie/controller/EmployeeController.java
+++ src/main/java/reggie/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package reggie.controller;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
@@ -82,7 +83,16 @@ public class EmployeeController {
      */
     @GetMapping("/page")
     public R<Page> page(int page,int pageSize,String name){
-
-        return null;
+        //分页构造器
+        Page pageInfo = new Page(page,pageSize);
+        //条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        //添加过滤条件
+        queryWrapper.like(StringUtils.isNotEmpty(name),Employee::getName,name);
+        //添加排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+        //执行查询
+        employeeService.page(pageInfo,queryWrapper);
+        return R.success(pageInfo);
     }
 }
